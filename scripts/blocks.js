83263ee6d7c28b28e490a1aaf7482f9341ea3eb5
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-07-29 21:03:03 +0200

    Create blocks.js

---
+++ scripts/blocks.js
@@ -0,0 +1,17 @@
+Events.on(ContentInitEvent, e => {
+    Vars.content.block("mindustry-rhyolite").attributes.set(Attribute.get("underground-steam"), 0.4);
+    Vars.content.block("mindustry-rhyolite-crater").attributes.set(Attribute.get("underground-steam"), 0.5);
+    Vars.content.block("mindustry-rough-rhyolite").attributes.set(Attribute.get("underground-steam"), 0.3);
+    Vars.content.block("mindustry-regolith").attributes.set(Attribute.get("underground-steam"), 0.1);
+    Vars.content.block("mindustry-yellow-stone").attributes.set(Attribute.get("underground-steam"), 0.9);
+    Vars.content.block("mindustry-carbon-stone").attributes.set(Attribute.get("underground-steam"), 0.1);
+    Vars.content.block("mindustry-beryllic-stone").attributes.set(Attribute.get("underground-steam"), 1);
+    Vars.content.block("mindustry-crystalline-stone").attributes.set(Attribute.get("underground-steam"), 0.7);
+    Vars.content.block("mindustry-crystal-floor").attributes.set(Attribute.get("underground-steam"), 0.5);
+    Vars.content.block("mindustry-yellow-stone-plates").attributes.set(Attribute.get("underground-steam"), 1);
+    Vars.content.block("mindustry-red-stone").attributes.set(Attribute.get("underground-steam"), 1.1);
+    Vars.content.block("mindustry-dense-red-stone").attributes.set(Attribute.get("underground-steam"), 0.9);
+    Vars.content.block("mindustry-red-ice").attributes.set(Attribute.get("underground-steam"), 1.3);
+    Vars.content.block("mindustry-arkyic-stone").attributes.set(Attribute.get("underground-steam"), 1.1);
+    Vars.content.block("mindustry-rhyolite").attributes.set(Attribute.get("underground-steam"), 0.4);
+});
