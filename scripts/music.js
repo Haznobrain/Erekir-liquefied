87886e08e1ee044a4efee06fa880a8be11fdff05
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-08-06 10:11:07 +0200

    Update music.js

--- scripts/music.js
+++ scripts/music.js
@@ -1,4 +1,4 @@
 Events.run(Trigger.update, () => {
     //check if unit type name is equal to name, then check if unit team is not the player team, then check if unit has guardian status
-    Groups.unit.find(e => e.type.name == "erekirliquefied-action" && e.team != Vars.player.team && e.hasEffect(StatusEffects.guardian)) Reflect.invoke(SoundControl, Vars.control.sound, "actio_venit", [myMusic], Music)
+    Groups.unit.find(e => e.type.name == "erekirliquefied-action" && e.team != Vars.player.team && e.hasEffect(StatusEffects.guardian)) Reflect.invoke(SoundControl, Vars.control.sound, "erekirliquefied-actio_venit", [myMusic], Music)
 })
